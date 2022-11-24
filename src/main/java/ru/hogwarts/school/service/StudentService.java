d8617644c98bab3ae445c28f58f6262b7366c416
Alex <alexeym75@gmail.com>
2022-11-24 21:22:52 +0700

    Added homework 3.2

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+    private final Map<Long, Student> students = new HashMap<>();
+    private long lastId;
+
+    public Student createStudent(Student student) {
+        student.setId(++lastId);
+        students.put(lastId, student);
+        return student;
+    }
+
+    public Student findStudent(long id) {
+        return students.get(id);
+    }
+
+    public Student editStudent(Student student) {
+        if (students.containsKey(student.getId())) {
+            students.put(student.getId(), student);
+            return student;
+        }
+        return null;
+    }
+
+    public Student deleteStudent(long id) {
+        return students.remove(id);
+    }
+
+    public Collection<Student> getStudentsByAge(int age) {
+        return students.entrySet().stream()
+                .filter(e -> e.getValue().getAge() == age)
+                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue())).values();
+    }
+}
